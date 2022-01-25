2bf3421bb50ddddb5334173f1536cf30541466f6
Tony Ware <ton8675309@gmail.com>
2022-01-25 11:41:40 -0700

    done

--- planIt/server/services/ProjectsService.js
+++ planIt/server/services/ProjectsService.js
@@ -4,13 +4,13 @@ import { BadRequest } from '../utils/Errors'
 class ProjectsService {
   // NOTE get all your projects
   async getAll() {
-    const projects = await dbContext.Projects.find().populate('creator')
+    const projects = await dbContext.Projects.find().populate('creator', 'name')
     return projects
   }
 
   // NOTE get on project by id
   async getById(id) {
-    const project = await dbContext.Projects.findById(id).populate('creator')
+    const project = await dbContext.Projects.findById(id).populate('creator', 'name')
     if (!project) {
       throw new BadRequest('Invalid Project Id')
     }
@@ -20,16 +20,17 @@ class ProjectsService {
   // NOTE create project
   async create(body) {
     const createdProject = await dbContext.Projects.create(body)
+    await createdProject.populate('creator', 'name picture')
     return createdProject
   }
 
   // NOTE delete project
-  async remove(projectId, userId) {
+  async remove(projectId, creatorId) {
     const original = await this.getById(projectId)
-    if (original.creatorId.toString() !== userId) {
+    if (original.creatorId.toString() !== creatorId) {
       throw new BadRequest('Cannot Delete')
     }
-    await dbContext.Projects.findOneAndRemove({ _id: projectId })
+    await dbContext.Projects.findOneAndRemove({ _id: projectId, creatorId: creatorId })
   }
 
 }
