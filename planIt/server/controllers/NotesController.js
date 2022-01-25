6814259f5797bfa7bbedd26664a1dd3c73011bdc
Zach <z6chy@icloud.com>
2022-01-25 10:55:39 -0700

    Notes Controller Done

--- planIt/server/controllers/NotesController.js
+++ planIt/server/controllers/NotesController.js
@@ -1,11 +1,43 @@
+import { notesService } from "../services/NotesService";
 import BaseController from "../utils/BaseController";
 
 export class NotesController extends BaseController {
   constructor() {
     super("api/projects/:projectId/notes")
     this.router
-    // TODO creat note
-    // TODO get notes by projectId
-    // TODO delete note
+      // NOTE get notes by projectId
+      .get('', this.getByProject)
+      // NOTE creat note
+      .post('', this.create)
+      // NOTE delete note
+      .delete('/:id', this.remove)
+  }
+
+  async getByProject(req, res, next) {
+    try {
+      const notes = await notesService.getByProject()
+      return res.send(notes)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async create(req, res, next) {
+    try {
+      req.body.creatorId = req.userInfo.creatorId
+      const note = await notesService.create(req.body)
+      return res.send(note)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async remove(req, res, next) {
+    try {
+      await notesService.remove(req.params.id, req.userInfo.id)
+      return res.send('Note was delorted')
+    } catch (error) {
+      next(error)
+    }
   }
 }
