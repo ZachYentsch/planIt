94549425c7fe9d9be38405429fb24d70f8a20f66
Tony Ware <ton8675309@gmail.com>
2022-01-25 10:54:19 -0700

    finished projects

--- planIt/server/controllers/ProjectsController.js
+++ planIt/server/controllers/ProjectsController.js
@@ -1,24 +1,57 @@
 import BaseController from "../utils/BaseController";
-
+import { projectsService } from "../services/ProjectsService";
 export class ProjectsController extends BaseController {
 
 
   constructor() {
     super("api/projects")
     this.router
+      // NOTE Get All projects is getUserProjects
       .get('', this.getUsersProjects)
-    // TODO get all your projects
-    // TODO get on project by id
-    // TODO create project
+
+      // NOTE get on project by id
+      .get('/:id', this.getProjectsById)
+      // NOTE create project
+      .post('', this.createProject)
     // TODO delete project
 
 
   }
+
   async getUsersProjects(req, res, next) {
     try {
-      const
+      const projects = await projectsService.getAll()
+      return res.send(projects)
     } catch (error) {
       next(error)
     }
   }
+
+  async getProjectsById(req, res, next) {
+    try {
+      const projectsId = await projectsService.getById(req.params.id)
+      return res.send(projectsId)
+    } catch (error) {
+      next(error)
+    }
+  }
+  async createProject(req, res, next) {
+    try {
+      req.body.creatorId = req.userInfo.id
+      const createdProject = await projectsService.create(req.body)
+      return res.send(createdProject)
+    } catch (error) {
+      next(error)
+    }
+
+  }
+  async removeProject(req, res, next) {
+
+    try {
+      await projectsService.remove(req.params.id, req.userInfo.id)
+    } catch (error) {
+      next(error)
+    }
+  }
+
 }
