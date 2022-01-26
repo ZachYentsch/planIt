7c90caa8d47d99362c070f32354d2647466466b8
Zach <z6chy@icloud.com>
2022-01-26 14:56:07 -0700

    tasks service get

--- planIt.client/src/services/TasksService.js
+++ planIt.client/src/services/TasksService.js
@@ -1,8 +1,12 @@
+import { AppState } from "../AppState"
+import { logger } from "../utils/Logger"
+import { api } from "./AxiosService"
+
 class TasksService {
-
-  async getTasks() {
-
-
+  async getTasks(projectId) {
+    const res = await api.get(`api/projects/${projectId}/tasks`)
+    logger.log(res.data)
+    AppState.tasks = res.data
   }
 }
 
