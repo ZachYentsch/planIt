055fe71670d85198303abdd544da26c581d71822
Tony Ware <ton8675309@gmail.com>
2022-01-26 14:59:56 -0700

    nots by get are done

--- planIt.client/src/services/NotesService.js
+++ planIt.client/src/services/NotesService.js
@@ -1,7 +1,13 @@
+import { AppState } from "../AppState"
+import { logger } from "../utils/Logger"
+import { api } from "./AxiosService"
+
 class NotesService {
 
-  async getNotes() {
-
+  async getNotes(projectId) {
+    const res = await api.get(`api/projects/${projectId}/notes`)
+    logger.log('notes res', res.data)
+    AppState.notes = res.data
   }
 }
 
