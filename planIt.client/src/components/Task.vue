fc73f5b48313850b88c2125d35073c5f0aa31ea1
Tony Ware <ton8675309@gmail.com>
2022-01-27 12:08:46 -0700

    0

--- planIt.client/src/components/Task.vue
+++ planIt.client/src/components/Task.vue
@@ -1,34 +1,42 @@
-<template>
+// <template>
+  //
   <div class="col-12">
+    //
     <div class="card-lg bg-dark mt-3">
+      //
       <div class="card-body selectable">
+        //
         <h1>{{ task }}</h1>
-        <button class="selectable btn btn-danger">Delete</button>
+        // <button class="selectable btn btn-danger">Delete</button> //
       </div>
+      //
     </div>
+    //
   </div>
+  //
 </template>
 
 
-<script>
-import { computed } from "@vue/reactivity"
-import { AppState } from "../AppState"
-export default {
-  props: {
-    task: {
-      type: object,
-      required: true
-    }
-  },
-  setup(props) {
+// <script>
+// import { computed } from "@vue/reactivity"
+// import { AppState } from "../AppState"
+// export default {
+//   props: {
+//     task: {
+//       type: object,
+//       required: true
+//     }
+// },
+// setup(props) {
 
-    return {
-      task: computed(() => AppState.task)
-    }
-  }
-}
-</script>
+//   return {
+//     task: computed(() => AppState.tasks)
+//   }
+// }
+// }
+// </script>
 
 
-<style lang="scss" scoped>
+// <style lang="scss" scoped>
+//
 </style>
