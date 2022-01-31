68758adde43d546c547de3e9a03d29eb08b15cf4
Zach <z6chy@icloud.com>
2022-01-30 22:58:21 -0700

    push

--- planIt.client/src/services/ProfilesService.js
+++ planIt.client/src/services/ProfilesService.js
@@ -4,6 +4,12 @@ import { api } from "./AxiosService"
 
 
 class ProfilesService {
+    async getProfile(id) {
+        const res = await api.get(`api/profiles/${id}`)
+        logger.log('profile Res', res.data)
+        AppState.profile = res.data
+    }
+
     async editProfile(updatedProfile) {
         const res = await api.put('account/', updatedProfile)
         logger.log(res.data)
